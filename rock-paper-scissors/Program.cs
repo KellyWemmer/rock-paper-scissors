90bf53e81685da0dec9782dba85891077d80d3f1
Kelly Wemmer <37150725+KellyWemmer@users.noreply.github.com>
2022-09-12 18:35:22 -0600

    Edits

--- rock-paper-scissors/Program.cs
+++ rock-paper-scissors/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Rock Paper Scissors");
 
 List<string> gestures = new List<string> {//List out result options
     "rock",
@@ -7,36 +6,50 @@ List<string> gestures = new List<string> {//List out result options
     "scissors"
 };
 
+Console.WriteLine("Welcome to Rock Paper Scissors");
+
 for (int i = 0; i < gestures.Count; i++)
 {
     Console.WriteLine(gestures[i]);
-}//Success!!
+}
+bool playing = true;
+do
+{
+    Random rand = new Random();
+    int randomIndex = rand.Next(0, gestures.Count - 1); //Returns a random int that <= 0 and count-1
+    string secretGesture = gestures[randomIndex];
 
-Random rand = new Random();
-int randomIndex = rand.Next(0, gestures.Count - 1); //Returns a random int that <= 0 and count-1
+    string? input = "";
+    input= Console.ReadLine();//This is where my guess is entered into memory
+    Console.WriteLine("The computer's answer was " + secretGesture + " Your answer was " + input);
 
-string secretGesture = gestures[randomIndex];
+    if (secretGesture == input)
+    {
+        Console.WriteLine("Try again");
+    }
+    else if(secretGesture == "rock" && input == "paper")
+    {
+        Console.WriteLine("You've Won!");
+    }
+    else if(secretGesture == "paper" && input == "scissors")
+    {
+        Console.WriteLine("You've Won!");
+    }
+    else if(secretGesture == "scissors" && input == "rock")
+    {
+        Console.WriteLine("You've Won!");
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you lost this game");
+    }
+} while(playing);
 
-string input = "";
 
-if (secretGesture == input)
-{
-    Console.WriteLine("Try again");
-}
-else if(secretGesture == "rock" && input == "paper")
-{
-    Console.WriteLine("You've Won!");
-}
-else if(secretGesture == "paper" && input == "scissors")
-{
-    Console.WriteLine("You've Won!");
-}
-else if(secretGesture == "scissors" && input == "rock")
-{
-    Console.WriteLine("You've Won!");
-}
-else
-{
-    Console.WriteLine("Sorry, you lost this game");
-}
+
+
+
+
+
+
 
